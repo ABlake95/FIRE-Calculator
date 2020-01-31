1a5537b1baff4bd0fa3f0448a3e784ca413beeb9
Alexander Blake <alex.blake24@gmail.com>
2020-01-30 18:18:01 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import numpy
 
 terminus = 90
-currentAge = 24
+currentAge = 25
 retirementAge = 30
-startingBalance = 100000
+startingBalance = 105823.95
 annualSpend = 24000
 inflation = 1.0225
-simulations = 1000
+simulations = 10000
 # returnStdev = 0.24907273550480202
 # returnAverage = 0.12393469387755104
-returnStdev = 0.3
-returnAverage = 0.19137
-contribution = 40000
+returnStdev = 0.35
+returnAverage = 0.2
+contribution = 24000
 
 success = 0
 bestCase = 0
@@ -51,7 +51,10 @@ for i in range(simulations):
         if bestCase == 0 or bestCase < portfolio:
             bestCase = portfolio
 
+bestCase /= (inflation ** (terminus - currentAge))
+
 print('Success Rate: ', success / simulations * 100, '%', sep = '')
-print('Best Case: ${:,}'.format(bestCase))
+print('Inflation-Adjusted Best Case: ${:,}'.format(bestCase))
 print('Worst Case:', worstCase, 'years')
-print('Median Case: ${:,}'.format(numpy.median(medianCase)))
+print('Inflation-Adjusted Median Case: ${:,}'.format(numpy.median(medianCase) / (inflation ** (terminus - currentAge))))
+
